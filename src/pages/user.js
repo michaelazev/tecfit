91cf8231b477fc57a662aaf490aabc16c0cfb1be
HenriqueChen <57077907+HenriqueChen@users.noreply.github.com>
2025-03-28 21:50:48 -0300

    trad

--- src/pages/user.js
+++ src/pages/user.js
@@ -7,29 +7,29 @@ function User() {
                 <div class="left-container">
                     <img className="imgProfile" src="https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__480.jpg" alt="Profile Image" />
                     <h2 class="gradienttext2">John Doe</h2>
-                    <p className="roleName">Web Developer</p>
+                    <p className="roleName">Bombado</p>
                 </div>
                 <div class="right-container">
-                    <h3 class="gradienttext">Profile Details</h3>
+                    <h3 class="gradienttext">Seu Perfil</h3>
                     <table>
                         <tr>
-                            <td>Name :</td>
+                            <td>Nome:</td>
                             <td>John Doe</td>
                         </tr>
                         <tr>
-                            <td>Age :</td>
+                            <td>Idade: </td>
                             <td>35</td>
                         </tr>
                         <tr>
-                            <td>Mobile :</td>
+                            <td>Celular:</td>
                             <td>+91 XXXXXXXXXX</td>
                         </tr>
                         <tr>
-                            <td>Email :</td>
+                            <td>Email:</td>
                             <td>john@example.com</td>
                         </tr>
                         <tr>
-                            <td>Address :</td>
+                            <td>Endereço:</td>
                             <td>123 Main St, Anytown, USA</td>
                         </tr>
                     </table>
