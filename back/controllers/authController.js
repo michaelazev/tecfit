e64884dc66972f3d6ae2601e063a1767151557b1
HenriqueChen <57077907+HenriqueChen@users.noreply.github.com>
2025-04-03 19:23:24 -0300

    a11

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -16,7 +16,9 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, '../config/DigiCertGlobalRootCA.crt.pem'))
+}
 });
 
 // Rota de registro
