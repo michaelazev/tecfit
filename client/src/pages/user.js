f160cf28273ed5b2eabbd4906e09c6b210461800
HenriqueChen <57077907+HenriqueChen@users.noreply.github.com>
2025-04-07 22:51:36 -0300

    pack1a

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./user.css";
+import backgroundImage from "../assets/background.jpg"; // Caminho relativo para a imagem
 
 function User() {
     const [userData, setUserData] = useState({});
 
     useEffect(() => {
-        // Recupera os dados do localStorage
         const storedUser = localStorage.getItem('user');
         if (storedUser) {
             setUserData(JSON.parse(storedUser));
@@ -13,7 +13,14 @@ function User() {
     }, []);
 
     return (
-        <div className="container">
+        <div
+            className="container"
+            style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+            }}
+        >
             <div className="card1">
                 <div className="left-container">
                     <img className="imgProfile" src="https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358__480.jpg" alt="Profile Image" />
