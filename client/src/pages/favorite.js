4839ce305e91a82dc62b4eef5209cffe3145d0e4
Henrique Chen <129029894+HenriqueChenWyden@users.noreply.github.com>
2025-05-23 09:40:13 -0300

    implementando lista de favorito

---
+++ client/src/pages/favorite.js
@@ -0,0 +1,53 @@
+import styles from "./Favorites.module.css";
+import React, { useState } from "react";
+
+function Favorite() {
+    // Estado para controlar a abertura do menu
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    // Função para alternar o estado do menu (aberto/fechado)
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        document.body.classList.toggle("open");  // Adiciona ou remove a classe "open" no body
+    };
+
+
+    return (
+        <div className="app">
+    {/* Barra de topo com o logotipo */}
+    <div className="barra-menu">
+        <div className="logo">
+            <img src="/img/tec_fit-removebg-preview.png" width="120" alt="Tec Fit Logo" />
+        </div>
+    </div>
+
+    {/* Botão para abrir/fechar o menu */}
+    <button className="Tec" onClick={toggleMenu}>
+        <span className="Tec-icon"></span>
+    </button>
+
+    {/* Menu lateral com links de navegação */}
+    <div className={`menu ${menuOpen ? "active" : ""}`}>
+        <nav>
+            <a href="/" style={{ animationDelay: "0.2s" }}>
+        <h6>Inicio</h6>
+            </a>
+            <a href="/sobre" style={{ animationDelay: "0.4s" }}>
+        <h6>Sobre</h6>
+            </a>
+            <a href="/contato" style={{ animationDelay: "0.6s" }}>
+        <h6>Usuário</h6>
+            </a>
+        </nav>
+    </div>
+
+    {/* Seção "Favoritos" */}
+    <container class="favoritos">
+            <h2>Favoritos</h2>
+            <h3>Lista de Favoritos</h3>
+    </container>
+        </div>
+    );  
+}
+
+export default Favorite;
